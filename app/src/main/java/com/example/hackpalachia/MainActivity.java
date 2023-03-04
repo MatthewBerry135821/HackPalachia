dd4619add2f2b16a97896aff2102aad38759b003
LukeGoodDay <107583117+LukeGoodDay@users.noreply.github.com>
2023-03-04 15:32:05 -0500

    I added a text box...

--- app/src/main/java/com/example/hackpalachia/MainActivity.java
+++ app/src/main/java/com/example/hackpalachia/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void onBtnClick (View view){
         TextView txtHello = findViewById(R.id.textHello);
-        txtHello.setText("Hello");
+        EditText editText = findViewById(R.id.editTextName);
+        txtHello.setText("Hello " + editText.getText().toString());
     }
 }
