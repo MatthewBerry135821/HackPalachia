591ab64b3747a988acfa1c193f2eaff0d089ec1b
LukeGoodDay <107583117+LukeGoodDay@users.noreply.github.com>
2023-03-04 17:14:32 -0500

    Listing stuff...

--- app/src/main/java/com/example/hackpalachia/MainActivity.java
+++ app/src/main/java/com/example/hackpalachia/MainActivity.java
@@ -4,20 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView)findViewById(R.id.listView);
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        arrayList.add("Luke");
+        arrayList.add("Matthew");
+        arrayList.add("Narayan");
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,arrayList);
+        listView.setAdapter(arrayAdapter);
     }
 
-    public void onBtnClick (View view){
-        TextView txtHello = findViewById(R.id.textHello);
-        EditText editText = findViewById(R.id.editTextName);
-        txtHello.setText("Hello " + editText.getText().toString());
-    }
+    /*protected void loadList(){
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,arrayList);
+        listView.setAdapter(arrayAdapter);
+    }*/
 }
